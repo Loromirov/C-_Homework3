584dd50572fb555fb1c1c619533f8dcc8c77a5eb
Loromirov <loromirov@yandex.ru>
2022-12-11 02:29:24 +0300

    первая задача

--- Program.cs
+++ Program.cs
@@ -1,27 +1,15 @@
-﻿/*Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+﻿/*Напишите программу, которая принимает на вход пятизначное 
+число и проверяет, является ли оно палиндромом.
 
-A (3,6,8); B (2,1,-7), -> 15.84
-
-A (7,-5, 0); B (1,-1,9) -> 11.53
+14212 -> нет
+12821 -> да
+23432 -> да
 */
-Console.WriteLine("Введите координаты X первой точки");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты Y первой точки");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты Z первой точки");
-int z1 = Convert.ToInt32(Console.ReadLine());
-
-
-Console.WriteLine("Введите координаты X второй точки");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты Y второй точки");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты Z второй точки");
-int z2 = Convert.ToInt32(Console.ReadLine());
-double Distxyz(int x1, int y1, int z1, int x2, int y2, int z2)
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+string str = num.ToString();
+if (str[0] == str[4] & str[1] == str[3])
 {
-    double result =0;
-    result = Math.Sqrt(Math.Pow((x1 - x2),2) + Math.Pow((y1 - y2),2) + Math.Pow((z1 - z2),2));
-    return result;
-}
-Console.WriteLine($"Расстояние между точками составляет {Distxyz(x1, y1, z1, x2, y2, z2)}");
+    Console.WriteLine("Да");
+} 
+else Console.WriteLine("нет");
